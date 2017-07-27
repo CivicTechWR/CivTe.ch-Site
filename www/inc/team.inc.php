b0f674d5f13338f68599ff1f25e34e3151770d65
Kristina Taylor <anitsirk@gmail.com>
2017-07-26 22:50:27 -0400

    update to add Kristina

--- www/inc/team.inc.php
+++ www/inc/team.inc.php
@@ -21,22 +21,22 @@
   				</div>
 			</div>
 			<div class="card">
-  				<img class="card-img-top" src="../images/jonathan.png" alt="Image of Jonathan Tonge">
+  				<img class="card-img-top" src="../images/kristina.jpg" alt="Image of Kristina Taylor">
   				<div class="card-block">
     					<h4 class="card-title">Kristina Taylor</h4>
     					<p class="card-text">Kristina is technical product manager at Tulip Retail.</p>
-    					<a class="btn btn-social-icon btn-twitter" href="https://twitter.com/civictechwr" target="_blank"><span class="fa fa-twitter"></span></a>
-			 	 	<a class="btn btn-social-icon btn-github" href="https://github.com/CivicTechWR" target="_blank"><span class="fa fa-github"></span></a>
-			  		<a class="btn btn-social-icon btn-linkedin" href="#" target="_blank"><span class="fa fa-linkedin"></span></a>
-					<a class="btn btn-social-icon btn-instagram" href="#" target="_blank"><span class="fa fa-instagram"></span></a>
+					<a class="btn btn-social-icon btn-twitter" href="https://twitter.com/anitsirKTaylor" target="_blank"><span class="fa fa-twitter"></span></a>
+			 	 	<a class="btn btn-social-icon btn-github" href="https://github.com/KristinaTaylor" target="_blank"><span class="fa fa-github"></span></a>
+			  		<a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/kristina-taylor/" target="_blank"><span class="fa fa-linkedin"></span></a>
+					<a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com/anitsirktaylor/" target="_blank"><span class="fa fa-instagram"></span></a>
 
   				</div>
 			</div>
 			<div class="card">
-  				<img class="card-img-top" src="../images/jonathan.png" alt="Image of Jonathan Tonge">
+  				<img class="card-img-top" src="../images/jonathan.png" alt="Image of ...">
   				<div class="card-block">
-    					<h4 class="card-title">Kristina Taylor</h4>
-    					<p class="card-text">Kristina is technical product manager at Tulip Retail.</p>
+    					<h4 class="card-title">...</h4>
+    					<p class="card-text">...</p>
     					<a class="btn btn-social-icon btn-github" href="https://github.com/CivicTechWR" target="_blank"><span class="fa fa-github"></span></a>
 			  		<a class="btn btn-social-icon btn-linkedin" href="#" target="_blank"><span class="fa fa-linkedin"></span></a>
 			  		<a class="btn btn-social-icon btn-flickr" href="#" target="_blank"><span class="fa fa-flickr"></span></a>
